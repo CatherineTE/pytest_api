d8daa14b8d0fbf6acc8255ae0ab4841953695795
CatherineTE <ekaluginaa@gmail.com>
2025-03-06 17:03:18 +0300

    test_api.py использует параметризацию для 2 методов и перенесен в отдельную папку, создан файл payloads

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,46 +1,28 @@
-import requests
+import pytest
 from endpoints.create_object import CreateObject
 from endpoints.delete_object import DeleteObject
 from endpoints.get_object import GetObject
 from endpoints.update_object import UpdateObject
+from tests.test_data import payloads
 
 
-payload = {
-      "name": "Apple MacBook Pro 17",
-      "data": {
-         "year": 2019,
-         "price": 2099.99,
-         "CPU model": "Intel Core i9",
-         "Hard disk size": "1 TB"
-      }
-    }
-
-def test_create_object():
+@pytest.mark.parametrize('payload', payloads.create_payloads)
+def test_create_object(payload):
     new_create_object = CreateObject()
     new_create_object.post(payload=payload)
     new_create_object.check_response_is_200()
     new_create_object.check_name(payload['name'])
 
 
-
 def test_get_object(obj_id):
     get_object = GetObject()
-    get_object.get_by_id(obj_id)
+    print(get_object.get_by_id(obj_id))
     get_object.check_response_is_200()
     get_object.check_response_id(obj_id)
 
-
-def test_update_object(obj_id):
+@pytest.mark.parametrize('payload', payloads.update_payloads)
+def test_update_object(obj_id, payload):
     update_object = UpdateObject()
-    payload = {
-        "name": "Apple MacBook Pro 18",
-        "data": {
-            "year": 2025,
-            "price": 2099.99,
-            "CPU model": "M10",
-            "Hard disk size": "1 TB"
-        }
-    }
     update_object.update_by_id(obj_id, payload)
     update_object.check_response_is_200()
     update_object.check_response_name(payload['name'])
---
+++ tests/test_data/payloads.py
@@ -0,0 +1,66 @@
+
+
+create_payloads = [
+    {
+      "name": "Apple MacBook Pro 17",
+      "data":
+          {
+            "year": 2019,
+            "price": 2099,
+            "CPU model": "Intel Core i9",
+            "Hard disk size": "1 TB"
+          }
+    },
+    {
+      "name": "Apple MacBook Pro 18",
+       "data":
+           {
+            "year": 2020,
+             "price": 2099.99,
+             "CPU model": "Intel Core i9hgjhhkjhkjhgjghvfgvcghvjhbgjhnhkjdfsdfxgfg hghjhjkhjkhgj",
+             "Hard disk size": "1 TB"
+            }
+        },
+    {
+      "name": "Apple MacBook Pro 19",
+      "data": {
+                "year": 2021,
+                "price": 1234,
+                "CPU model": "!@#$%^&*()",
+                "Hard disk size": "1 TB"
+              }
+    }
+]
+
+update_payloads = [
+    {
+      "name": "Apple MacBook Pro 27",
+      "data":
+          {
+         "year": 2021,
+         "price": 3099,
+         "CPU model": "Intel Core i9",
+         "Hard disk size": "1 TB"
+      }
+    },
+    {
+          "name": "Apple MacBook Pro 28",
+          "data":
+              {
+             "year": 2020,
+             "price": 2099.99,
+             "CPU model": "Intel Core i9hgjhhkjhkjhgjghvfgvcghvjhbgjhnhkjdfsdfxgfg hghjhjkhjkhgj",
+             "Hard disk size": "1 TB"
+          }
+        },
+    {
+      "name": "Apple MacBook Pro 29",
+      "data":
+          {
+            "year": 2021,
+            "price": 1234,
+            "CPU model": "!@#$%^&*()",
+            "Hard disk size": "1 TB"
+      }
+    }
+]
